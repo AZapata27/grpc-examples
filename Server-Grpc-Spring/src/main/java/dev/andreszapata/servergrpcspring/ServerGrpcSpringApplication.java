434e5c4295206716124e711536bab17f504d7a20
Andres Zapata <andres.zapata@oljsolutions.com>
2024-04-17 11:26:01 -0500

    Init Grpc Server Spring Boot

---
+++ Server-Grpc-Spring/src/main/java/dev/andreszapata/servergrpcspring/ServerGrpcSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.andreszapata.servergrpcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGrpcSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGrpcSpringApplication.class, args);
+    }
+
+}
---
+++ Server-Grpc-Spring/src/test/java/dev/andreszapata/servergrpcspring/ServerGrpcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andreszapata.servergrpcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGrpcSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
